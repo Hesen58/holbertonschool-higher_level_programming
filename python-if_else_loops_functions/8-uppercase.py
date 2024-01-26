fb4967289f9f8827e8514bde9e5fc449521ba487
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-26 04:08:55 -0800

    a

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
+def uppercase(strzor):
+    for i in strzor:
         if ord(i) > 96 and ord(i) < 123:
             print("{}".format(chr(ord(i) - 32)), end="")
         else:
