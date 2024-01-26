e44dfaf2e445858831c7906699f312f1e4f3ec7d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-26 03:00:23 -0800

    a

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i != 99 and i < 10:
-        print(f"0{i}, ")
+        print(f"0{i}, ", end="")
     elif i != 99 and i > 10:
-        print(f"{i}, ")
+        print(f"{i}, ", end="")
     else:
         print(f"{i}")
