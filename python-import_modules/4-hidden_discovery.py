8cd14694975b0f19bba867e0c8d339cb4703cb42
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-28 04:36:19 -0800

    a

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     zor = dir(hidden_4)
     for i in zor:
-        if (!(i[0] == "_" and i[1] == "_")):
+        if (i[0] != "_" or i[1] != "_"):
             print("{}".format(i))
