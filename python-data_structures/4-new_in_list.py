03d436af4946e69f7022e82e8415de09ec6d2475
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-28 05:15:02 -0800

    a

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    zor = my_list
+    zor = []
+    for i in range(len(my_list)):
+        zor.append(my_list[i])
     zor[idx] = element
     return zor
