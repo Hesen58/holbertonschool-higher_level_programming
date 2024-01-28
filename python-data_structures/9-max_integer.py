be0fb6be9c36df223b6c2119fe479895b2142156
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-28 06:13:20 -0800

    a

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     zor = 0
     for i in my_list:
-        if my_list[i] > zor:
-            zor = my_list[i]
+        if i > zor:
+            zor = i
     return zor
