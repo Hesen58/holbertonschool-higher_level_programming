9eb2c2ca2d9c5de6563740a93bf1f8c4122781f9
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-28 05:54:22 -0800

    a

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     zor1 = tuple_a + (0, 0)
     zor2 = tuple_b + (0, 0)
-    zor = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    zor = (zor1[0] + zor2[0], zor1[1] + zor2[1])
     return zor
