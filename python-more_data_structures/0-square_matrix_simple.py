f30aefdc47858b08d0e31abeba392ed41c152b48
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-30 03:47:03 -0800

    a

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    zor = [[]]
+    zor = []
     for i in range(0, len(matrix)):
-        for j in range(0, matrix[i]):
-            zor[i][j] = matrix[i][j] ** 2
+        zor.append([])
+        for j in range(0, len(matrix[i])):
+            zor[i].append(matrix[i][j] ** 2)
     return zor
