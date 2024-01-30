1f7eef79b3ea0e29b94fb11449a7573c32a7e5fd
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-30 06:36:35 -0800

    a

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    zor = map(lambda a: a + number, my_list)
-    return list(zor)
+    return list(map(lambda a: a * number, my_list))
     
