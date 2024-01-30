ed2c50075f8f49fe2cc684f4d3e432c83237b22e
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-30 06:09:54 -0800

    a

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return None
     zor = list(a_dictionary.values())[0]
+    hoqqa = ""
     for i in a_dictionary:
         if a_dictionary[i] > zor:
             zor = a_dictionary[i]
-    return zor
+            hoqqa = i
+    return hoqqa
