56691929119312605d30fdbee95b4c756ef7f5b4
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-03 11:12:39 -0800

    a

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    zor = None
     try:
         zor = a / b
-    except (TypeError, ValueError, ZeroDivisionError, UnboundLocalError):
+    except (TypeError, ValueError, ZeroDivisionError):
         return None
     finally:
         print("Inside result: {}".format(zor))
