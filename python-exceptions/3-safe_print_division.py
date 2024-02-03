1a82b7dd730184bf05d94f51d9dca0f8992c2771
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-03 11:09:42 -0800

    a

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        zor = a / b
+    except (TypeError, ValueError, ZeroDivisionError):
+        return None
+    finally:
+        print("Inside result: {}".format(zor))
+    return zor
