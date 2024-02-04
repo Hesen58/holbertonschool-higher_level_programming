06dbf657a781717cff60eca74383f520e5278ef3
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-04 09:39:13 -0800

    a

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+class Square:
+    '''Something useful'''
+    def __init__(self, size):
+        '''Something useful
+        Args:
+        size: Something more useful
+        '''
+        self.__size = size
