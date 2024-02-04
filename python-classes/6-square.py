934172d73643bd820ae17553e672e3bd02d2c9cf
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-04 11:02:09 -0800

    a

---
+++ python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+class Square:
+    '''Something usefull
+
+    Args:
+    size: Something more useful
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(self.__position[0] * " " + "#" * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not (all([isinstance(zor, int) for zor in value]) and value[0] >= 0 and value[1] >= 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not (isinstance(value, tuple) and len(value) == 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
