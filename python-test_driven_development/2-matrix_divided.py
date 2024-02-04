f9962916aeaecf2d88a03fac902ca8af30688f7d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-04 12:09:59 -0800

    a

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,17 +9,20 @@ def matrix_divided(matrix, div):
     div: Something more useful
     '''
     zor = []
+    zori = 0
+    oldrow = matrxi[0]
     if not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for i in range(len(matrix)):
-        if i != len(matrix[i]) - 1:
-            if not (len(matrix[i]) == len(matrix[i + 1])):
+    for row in matrix:
+            if not (len(oldrow) == len(row):
                 raise TypeError("Each row of the matrix must have the same size")
         zor.append([])
-        for node in matrix[i]:
+        for node in row:
+            oldrow = row
             if not (isinstance(node, int) or isinstance(node, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            zor[i].append(round(node / div, 2))
+            zor[zori].append(round(node / div, 2))
+            zori += 1
     return zor
