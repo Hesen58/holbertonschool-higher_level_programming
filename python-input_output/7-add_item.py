9c3e6e302479b4ace166964ebc56bdb943d3a34f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-11 01:59:11 -0800

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,8 +4,6 @@ import sys, json
 savejson = __import__('5-save_to_json_file').save_to_json_file
 loadjson = __import__('6-load_from_json_file').load_from_json_file
 
-
-zor = loadjson("add_item.json")
-if not zor:
-    zor == []
+try:
+    zor = loadjson("add_item.json")
 savejson(zor + sys.argv[1:], "add_item.json")
