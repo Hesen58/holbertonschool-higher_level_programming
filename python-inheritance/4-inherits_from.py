0b213d003f017cf0e487be0d0d7a900da5e265a0
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-10 23:54:22 -0800

    a

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+def inherits_from(obj, a_class):
+    '''Something more useful'''
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
