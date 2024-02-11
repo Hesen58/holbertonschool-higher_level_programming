2020f228f89ff9d584aef9e8669aad578c5c54d1
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-11 02:36:25 -0800

    a

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+class Student:
+    '''Something more useful'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        zor = {}
+        zorkeys = list(self.__dict__.keys())
+        if attrs is not None and all(isinstance(item, str) for item in attrs):
+            for i in attrs:
+                for j in zorkeys:
+                    if i == j:
+                        zor[i] = self.__dict__[j]
+            return zor
+        else:
+            return self.__dict__
