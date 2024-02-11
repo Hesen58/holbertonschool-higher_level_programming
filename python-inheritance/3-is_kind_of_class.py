259dfa832388940af44b5e7ec2d407df84d65317
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-10 23:51:08 -0800

    a

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Something more useful'''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
