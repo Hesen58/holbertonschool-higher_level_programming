cf2066413ac9ba5a162ae97bf4e96fa12366c227
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-10 23:45:38 -0800

    a

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Something useful'''
+
+
+def is_same_class(obj, a_class):
+    '''Something more useful'''
+    if isintance(obj, a_class):
+        return True
+    else:
+        return False
