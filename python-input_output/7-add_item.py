e9ce80221682d4b01db4984adce3c55e37bd77f1
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-11 02:01:37 -0800

    a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''Something useful'''
-import sys, json
+import sys
+import json
 savejson = __import__('5-save_to_json_file').save_to_json_file
 loadjson = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     zor = loadjson("add_item.json")
-except:
+except Exception:
     zor = []
 savejson(zor + sys.argv[1:], "add_item.json")
