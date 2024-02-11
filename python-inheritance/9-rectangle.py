0da93aa4468fd293fcfcebf0cfd4c3b2cd4dc436
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-11 00:31:59 -0800

    a

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,4 +15,4 @@ class Rectangle(Base):
         return self.__height * self.__width
 
     def __str__(self):
-        return "[{}] {}/{}".format(Base.name, self.__width, self.__height)
+        return "[{}] {}/{}".format(Base.__name__, self.__width, self.__height)
