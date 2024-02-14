fc0839853d81633095dc3239bb8a932f195ca54a
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-14 06:04:56 -0800

    a

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Something useful'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Something more useful'''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        zor_name = self.__class_.__name__
+        return "[{}] ({}) {}/{} - {}".format(
+            zor_name, self.id, self.x, self.y, self.size
+        )
