e979d3ecde110711403b200653ebcd4a64dd606f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-02-18 07:35:54 -0800

    a

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,3 +32,13 @@ class Square(Rectangle):
         else:
             for j in kwargs.keys():
                 setattr(self, j, kwargs[j])
+
+    def to_dictionary(self):
+        '''Something much more usegul'''
+        zor_dict = {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+        return zor_dict
