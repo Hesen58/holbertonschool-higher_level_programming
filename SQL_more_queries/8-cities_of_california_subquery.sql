0f7da83a39bce8808ea70c9052b1abd6b17a5c54
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 17:03:51 +0400

    8th one

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Something useful
-SELECT id, name FROM cities WHERE states.name = "California" order by id asc
+SELECT id, name FROM cities WHERE state_id in (SELECT id FROM states WHERE name = "California") order by id asc
