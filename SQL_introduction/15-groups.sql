1d6201855f7994270373db783fc4688ace016e65
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 16:11:41 +0400

    15th one

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Something useful
-SELECT score, COUNT(score) as number FROM second_table group by score 
+SELECT score, COUNT(score) as number FROM second_table group by score order by number desc
