cdb0755acc6d4c838dcf229a82a468344fe1f25d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 16:51:49 +0400

    6th one

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Something useful
 create database if not exists hbtn_0d_usa;
+USE hbtn_0d_usa;
 create table if not exists states (id int unique auto_increment not null primary key, name varchar(256) not null)
