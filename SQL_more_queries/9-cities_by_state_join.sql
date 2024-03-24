639dddcececf7209f7c7dd55e63d17adbe3cbd91
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 17:10:35 +0400

    9th one

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Something useful
-SELECT id, name, states.name as name FROM cities INNER JOIN states ON states.id = cities.state_id order by cities.id asc
+SELECT cities.id as id, cities.name as id, states.name as name FROM cities JOIN states ON states.id = cities.state_id order by cities.id asc
