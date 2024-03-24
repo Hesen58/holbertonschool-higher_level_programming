b4f70141dad793428fa2964480d2bce7ba1b49b8
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 17:40:12 +0400

    11th one

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- Something useful
-SELECT sol.title, sag.genre_id FROM tv_shows sol left join tv_show_genres sag on sag.show_id = sol.id order by sol.title, sag.genre_id asc
+SELECT sol.title, sag.genre_id FROM tv_shows sol LEFT JOIN tv_show_genres sag ON sag.show_id = sol.id order by sol.title, sag.genre_id asc
