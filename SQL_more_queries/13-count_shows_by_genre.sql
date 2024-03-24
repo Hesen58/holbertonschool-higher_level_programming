b95cf8e9afc92fd1765bf2559665083813f952f9
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 17:51:49 +0400

    13th one

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Something useful
+SELECT sol.name AS genre, count(sag.show_id) as number_of_shows FROM tv_genres sol RIGHT JOIN tv_show_genres sag ON sag.genre_id = sol.id group by genre order by number_of_shows desc
