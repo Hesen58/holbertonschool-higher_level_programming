38d33ec721c97527d6700630693eabdacd4907fe
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-24 15:52:38 +0400

    9th one

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 create table if not exists second_table (id int, name varchar(256), score int);
 insert into second_table (id, naame, score)
 values
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8);
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
